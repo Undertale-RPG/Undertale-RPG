12ea7a4a0c2d79daeae63e8c3b2db4e089ff0e5a
yakirarage <yaranaafs@gmail.com>
2022-08-02 23:29:26 +0200

    more stuff

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import disnake
+from dotenv import load_dotenv
 from disnake.ext import commands
+
+load_dotenv()
 
 description = """The undertale RPG Beta bot."""
 
@@ -11,20 +14,25 @@ intents.message_content = True
 class UndertaleBot(commands.AutoShardedBot):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.guilds_db = None
+        self.players = None
+        self.cluster = None
+        self.db = None
+        self.BotToken = os.getenv("TOKEN")
         self.invite_url = "https://discord.gg/FQYVpuNz4Q"
         self.vote_url = "https://top.gg/bot/815153881217892372"
         self.patreon_link = "https://www.patreon.com/undertale_rpg"
         self.currency = ":coin:"
         self.activity = disnake.Game("Undertale | /help ")
         self.help_command = None
-        
-    #async def on_shard_connect(self, shard):
-    #    print(
-    #        f"----------Shard {shard} is on.-------------\n"
-    #        f"Total Guilds: {len(self.guilds)}\n"
-    #        f"Total Shards: {len(self.shards)}\n"
-    #        f"-------------------------------------------"
-    #    )
+
+    async def on_shard_connect(self, shard):
+        print(
+            f"----------Shard {shard} is on.-------------\n"
+            f"Total Guilds: {len(self.guilds)}\n"
+            f"Total Shards: {len(self.shards)}\n"
+            f"-------------------------------------------"
+        )
 
     def load_all_cogs(self):
         for filename in os.listdir("./cogs"):
@@ -35,7 +43,6 @@ class UndertaleBot(commands.AutoShardedBot):
 
 
 bot = UndertaleBot(
-    command_prefix=commands.when_mentioned_or("?"),
     description=description,
     intents=intents,
     owner_ids=[536538183555481601, 513351917481623572]
@@ -43,4 +50,4 @@ bot = UndertaleBot(
 
 
 bot.load_all_cogs()
-bot.run("ODcxNzU2NzgyMDEwMjY1NjAw.GpSs42.p4uceF-9m68djR9N1a4HQ7sjh39Ur28AX-EZ1s")
+bot.run(bot.BotToken)
