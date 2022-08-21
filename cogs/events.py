83ba3615acf101e71d956f4fe30a67612f00c950
yakirarage <yaranaafs@gmail.com>
2022-08-21 22:33:39 +0200

    stuffs

--- cogs/events.py
+++ cogs/events.py
@@ -1,4 +1,5 @@
-from disnake.ext import commands
+from disnake.ext import commands, tasks
+from utility.dataIO import fileIO
 
 class Event(commands.Cog):
     def __init__(self, bot):
@@ -7,6 +8,16 @@ class Event(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print("im ready!!!!!!")
+
+    @tasks.loop(seconds=5)
+    async def data_task(self):
+        #self.bot.items = fileIO("data/items/items.json", "load")
+        #self.bot.monsters = fileIO("data/stats/monsters.json", "load")
+        #self.bot.locations = fileIO("data/traveling.json", "load")
+        self.bot.crates = fileIO("data/crates.json", "load")
+        #self.bot.shopping = fileIO("data/shops.json", "load")
+        self.bot.boosters = await self.bot.db["boosters"].find_one({"_id": 0})
+        self.bot.levels = fileIO("data/levels.json", "load")
 
 
 def setup(bot):
