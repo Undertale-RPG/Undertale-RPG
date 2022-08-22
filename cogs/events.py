2b5afce1afb8c08918a557faaa856bdc48f83b1c
yakirarage <yaranaafs@gmail.com>
2022-08-22 15:19:07 +0200

    added join message

--- cogs/events.py
+++ cogs/events.py
@@ -19,6 +19,14 @@ class Event(commands.Cog):
         self.bot.boosters = await self.bot.db["boosters"].find_one({"_id": 0})
         self.bot.levels = fileIO("data/levels.json", "load")
 
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        for channel in guild.text_channels:
+            if channel.permissions_for(guild.me).send_messages:
+                await channel.send(
+                    "Hello!, Thanks for adding me! You can use the command **/tutorial** To "
+                    "know how the bot works!")
+                break
 
 def setup(bot):
     bot.add_cog(Event(bot))
