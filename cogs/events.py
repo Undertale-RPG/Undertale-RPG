a9228ffabd155722abb003082dc3dc7dd9ba0ba3
yakirarage <yaranaafs@gmail.com>
2022-09-03 21:11:56 +0200

    Update events.py

--- cogs/events.py
+++ cogs/events.py
@@ -12,12 +12,13 @@ class Event(commands.Cog):
     @tasks.loop(seconds=5)
     async def data_task(self):
         #self.bot.items = fileIO("data/items/items.json", "load")
-        #self.bot.monsters = fileIO("data/stats/monsters.json", "load")
-        #self.bot.locations = fileIO("data/traveling.json", "load")
+        self.bot.monsters = fileIO("data/monsters.json", "load")
+        self.bot.locations = fileIO("data/locations.json", "load")
         self.bot.crates = fileIO("data/crates.json", "load")
         #self.bot.shopping = fileIO("data/shops.json", "load")
         self.bot.boosters = await self.bot.db["boosters"].find_one({"_id": 0})
         self.bot.levels = fileIO("data/levels.json", "load")
+        self.bot.bosses = fileIO("data/bosses.json", "load")
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
