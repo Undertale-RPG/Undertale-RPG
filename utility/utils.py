49fa646558a2883b5e6d4b71bde1910f245e3dd7
yakirarage <yaranaafs@gmail.com>
2022-09-03 21:50:41 +0200

    Update utils.py

--- utility/utils.py
+++ utility/utils.py
@@ -20,7 +20,7 @@ def in_battle():
         if str(inter.author.id) in inter.bot.fights:
             embed = disnake.Embed(
                 title="You have a fight dialogue open",
-                description=f"[Click here]({inter.bot.fights[str(inter.author.id)].msg.jump_url})",
+                description=f"> [Click here]({inter.bot.fights[str(inter.author.id)].msg.jump_url})",
                 color=disnake.Color.random()
             )
             await inter.send(embed=embed)
@@ -35,7 +35,7 @@ def in_shop():
         if str(inter.author.id) in inter.bot.shops:
             embed = disnake.Embed(
                 title="You have a shop dialogue open",
-                description=f"[Click here]({inter.bot.shops[str(inter.author.id)].msg.jump_url})",
+                description=f"> [Click here]({inter.bot.shops[str(inter.author.id)].msg.jump_url})",
                 color=disnake.Color.random()
             )
             await inter.send(embed=embed)
@@ -61,7 +61,7 @@ async def create_player_info(ctx, mem):
             "multi_g": 1,
             "multi_xp": 1,
             "tokens": 0,
-            # "damage": 0,
+            "damage": 0,
 
             "exp": 0,
             "gold": 200,
@@ -117,35 +117,35 @@ def occurrence(stored, value):
         return
 
 
-async def get_bar(health, max_health):
-    bar0 = "<:0_:899376245496758343>"
-    bar2 = "<:2_:899376429568000040>"
-    bar3 = "<:3_:899376559700451379>"
-    bar4 = "<:4_:899376608220172339>"
-    bar5 = "<:5_:899376657759088750>"
-    bar = None
-    mix = health / max_health
-    per = mix * 100
-    if per == 0:
-        bar = f"{bar0}{bar0}{bar0}{bar0}{bar0}"
-    if 10 >= per > 0:
-        bar = f"{bar2}{bar0}{bar0}{bar0}{bar0}"
-    if 20 >= per > 10:
-        bar = f"{bar5}{bar0}{bar0}{bar0}{bar0}"
-    if 30 >= per > 20:
-        bar = f"{bar5}{bar2}{bar0}{bar0}{bar0}"
-    if 40 >= per > 30:
-        bar = f"{bar5}{bar4}{bar0}{bar0}{bar0}"
-    if 50 >= per > 40:
-        bar = f"{bar5}{bar5}{bar2}{bar0}{bar0}"
-    if 60 >= per > 50:
-        bar = f"{bar5}{bar5}{bar4}{bar0}{bar0}"
-    if 70 >= per > 60:
-        bar = f"{bar5}{bar5}{bar5}{bar3}{bar0}"
-    if 80 >= per > 70:
-        bar = f"{bar5}{bar5}{bar5}{bar5}{bar2}"
-    if 90 >= per > 80:
-        bar = f"{bar5}{bar5}{bar5}{bar5}{bar4}"
-    if 100 >= per > 90:
-        bar = f"{bar5}{bar5}{bar5}{bar5}{bar5}"
-    return bar
+#async def get_bar(health, max_health):
+#    bar0 = "<:0_:899376245496758343>"
+#    bar2 = "<:2_:899376429568000040>"
+#    bar3 = "<:3_:899376559700451379>"
+#    bar4 = "<:4_:899376608220172339>"
+#    bar5 = "<:5_:899376657759088750>"
+#    bar = None
+#    mix = health / max_health
+#    per = mix * 100
+#    if per == 0:
+#        bar = f"{bar0}{bar0}{bar0}{bar0}{bar0}"
+#    if 10 >= per > 0:
+#        bar = f"{bar2}{bar0}{bar0}{bar0}{bar0}"
+#    if 20 >= per > 10:
+#        bar = f"{bar5}{bar0}{bar0}{bar0}{bar0}"
+#    if 30 >= per > 20:
+#        bar = f"{bar5}{bar2}{bar0}{bar0}{bar0}"
+#    if 40 >= per > 30:
+#        bar = f"{bar5}{bar4}{bar0}{bar0}{bar0}"
+#    if 50 >= per > 40:
+#        bar = f"{bar5}{bar5}{bar2}{bar0}{bar0}"
+#    if 60 >= per > 50:
+#        bar = f"{bar5}{bar5}{bar4}{bar0}{bar0}"
+#    if 70 >= per > 60:
+#        bar = f"{bar5}{bar5}{bar5}{bar3}{bar0}"
+#    if 80 >= per > 70:
+#        bar = f"{bar5}{bar5}{bar5}{bar5}{bar2}"
+#    if 90 >= per > 80:
+#        bar = f"{bar5}{bar5}{bar5}{bar5}{bar4}"
+#    if 100 >= per > 90:
+#        bar = f"{bar5}{bar5}{bar5}{bar5}{bar5}"
+#    return bar
