9ded07bf134e780111a447e41f88a76ed46cdd5d
yakirarage <yaranaafs@gmail.com>
2023-01-08 11:06:28 +0100

    Update webhook.py

--- cogs/webhook.py
+++ cogs/webhook.py
@@ -15,33 +15,35 @@ class TopGG(commands.Cog):
 
     @commands.Cog.listener()
     async def on_dbl_vote(self, data):
-        vote_data = data
-        voter = await self.bot.fetch_user(vote_data["user"])
-        info = await self.bot.players.find_one({"_id": voter.id})
-        if info is None:
-            print(f"{ConsoleColors.WARNING} This user is not registered.{ConsoleColors.ENDC}")
-            return
-        info["gold"] = info["gold"] + 500
-        info["standard crate"] += 1
-        info["last_voted"] = time.time()
-        #await self.bot.players.update_one({"_id": voter.id}, {"$set": info})
-        print(f"{ConsoleColors.GREEN}Received a vote from {str(voter)}, They got their rewards successfully{ConsoleColors.ENDC}")
+        print("this is the real thing it works!")
+        #vote_data = data
+        #voter = await self.bot.fetch_user(vote_data["user"])
+        #info = await self.bot.players.find_one({"_id": voter.id})
+        #if info is None:
+        #    print(f"{ConsoleColors.WARNING} This user is not registered.{ConsoleColors.ENDC}")
+        #    return
+        #info["gold"] = info["gold"] + 300
+        #info["standard crate"] += 1
+        #info["last_voted"] = time.time()
+#        #await self.bot.players.update_one({"_id": voter.id}, {"$set": info})
+        #print(f"{ConsoleColors.GREEN}Received a vote from {str(voter)}, They got their rewards successfully{ConsoleColors.ENDC}")
 
-        cha = self.bot.get_channel(1010596574155522078)
-        await cha.send(content=f"someone voted")
+        #cha = self.bot.get_channel(1010596574155522078)
+        #await cha.send(content=f"someone voted")
 
     @commands.Cog.listener()
     async def on_dbl_test(self, data):
-        vote_data = data
-        voter = await self.bot.fetch_user(vote_data["user"])
-        info = await self.bot.players.find_one({"_id": voter.id})
-        info["gold"] = info["gold"] + 500
-        info["standard crate"] += 1
-        #await self.bot.players.update_one({"_id": voter.id}, {"$set": info})
-        print(f"{ConsoleColors.GREEN}Received a vote from {str(voter)}, They got their rewards successfully{ConsoleColors.ENDC}")
-
-        cha = self.bot.get_channel(1010596574155522078)
-        await cha.send(content=f"someone voted!!")
+        print("this is a test it works!")
+        #vote_data = data
+        #voter = await self.bot.fetch_user(vote_data["user"])
+        #info = await self.bot.players.find_one({"_id": voter.id})
+        #info["gold"] = info["gold"] + 500
+        #info["standard crate"] += 1
+        ##await self.bot.players.update_one({"_id": voter.id}, {"$set": info})
+        #print(f"{ConsoleColors.GREEN}Received a vote from {str(voter)}, They got their rewards successfully{ConsoleColors.ENDC}")
+#
+        #cha = self.bot.get_channel(1010596574155522078)
+        #await cha.send(content=f"someone voted!!")
 
 def setup(bot):
     bot.add_cog(TopGG(bot))
