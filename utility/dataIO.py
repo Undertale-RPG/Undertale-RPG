ee4ae5438f2b55c8a8923a342dc7b253cff6bf8f
Floris Fiedeldij Dop <mrfloris@gmail.com>
2024-02-28 19:55:28 +0100

    - added docstring, and a suggestion, and JSON uppercases

--- utility/dataIO.py
+++ utility/dataIO.py
@@ -9,11 +9,19 @@ class InvalidFileIO(Exception):
 
 
 class DataIO:
+    """Class for automatic JSON file operations.
+
+    This class provides methods for automatic saving and loading of JSON files,
+    ensuring that the original file is unaltered if an error occurs during the process.
+
+    Attributes:
+        logger (Logger): The logger instance for logging messages related to file operations.
+    """
     def __init__(self):
         self.logger = logging.getLogger("red")
 
     def load_json(self, filename):
-        """Loads json file"""
+        """Loads JSON file"""
         return self._read_json(filename)
 
     def _read_json(self, filename):
@@ -22,7 +30,7 @@ class DataIO:
         return data
 
     def _legacy_fileio(self, filename, IO, data=None):
-        """Old fileIO provided for backwards compatibility"""
+        """Legacy fileIO provided for backwards compatibility"""
         if IO == "load" and data is None:
             return self.load_json(filename)
         elif IO == "check" and data is None:
@@ -38,7 +46,7 @@ def get_value(filename, key):
 
 
 def set_value(filename, key, value):
-    data = fileIO(filename, "load")
+    data = fileIO(filename, "load") # <-- Unused Variable in set_value function? The data variable in the set_value function is assigned the result of fileIO(filename, "load") but never used. You can omit that line if it's not necessary. Worth testing first though.
     data[key] = value
     fileIO(filename, "save", data)
     return True
